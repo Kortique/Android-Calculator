7a222241f8dee2c563cdac19b2b1717f53f4eab3
Kortique <kolobOK8>
2021-06-06 12:35:49 +0300

    Реализовал сложение, тестовое поле для проверки

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,23 +1,21 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import com.example.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    StringBuilder value1 = new StringBuilder("");
-    StringBuilder value2 = new StringBuilder("");
+    private StringBuilder value1 = new StringBuilder("");
+    private StringBuilder value2 = new StringBuilder("");
+    private String sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater()
-        );
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.button0.setOnClickListener(v -> setField("0"));
@@ -34,48 +32,53 @@ public class MainActivity extends AppCompatActivity {
         binding.buttonMinus.setOnClickListener(v -> operationSubtract());
         binding.buttonMultiply.setOnClickListener(v -> operationMultiply());
         binding.buttonDiv.setOnClickListener(v -> operationDivide());
-        binding.buttonEqual.setOnClickListener(v -> operationResult());
+        binding.buttonEqual.setOnClickListener(v -> operationEqual());
     }
 
     @SuppressLint("SetTextI18n")
     private void setField(String symbol) {
-        binding.result.setText(value1 + symbol);
+        binding.textView.setText(value1 + symbol);
         value1.append(symbol);
-    }
-
-    @SuppressLint("SetTextI18n")
-    private void operationSubtract() {
-        binding.result.setText(value1 + "-");
-        value2 = value1;
-        value1.append("-");
-    }
-
-    @SuppressLint("SetTextI18n")
-    private void operationMultiply() {
-        binding.result.setText(value1 + "*");
-        value2 = value1;
-        value1.append("*");
-    }
-
-    @SuppressLint("SetTextI18n")
-    private void operationDivide() {
-        binding.result.setText(value1 + "/");
-        value2 = value1;
-        value1.append("/");
+        binding.textView2.setText(value1);
+        binding.textView3.setText(value2);
     }
 
     @SuppressLint("SetTextI18n")
     private void operationAdd() {
-        binding.result.setText(value1 + "+");
-        value2 = value1;
-        value1.append("+");
+        sign = "+";
+        value2.append(value1);
+        value1.delete(0,value1.length());
     }
 
-    private void operationResult() {
+    @SuppressLint("SetTextI18n")
+    private void operationSubtract() {
+        sign = "-";
+        value2.append(value1);
+        value1.delete(0,value1.length());
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void operationMultiply() {
+        sign = "*";
+        value2.append(value1);
+        value1.delete(0,value1.length());
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void operationDivide() {
+        sign = "/";
+        value2.append(value1);
+        value1.delete(0,value1.length());
+    }
+
+    private void operationEqual() {
         double num1 = Double.parseDouble(String.valueOf(value1));
         double num2 = Double.parseDouble(String.valueOf(value2));
         double result = num1 + num2;
-        binding.result.setText((int) result);
+        binding.textView.setText(String.valueOf(result));
+        value1.delete(0,value1.length());
+        value2.delete(0,value2.length());
+        value2.append(result);
     }
 
 
